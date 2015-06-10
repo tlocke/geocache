2ccde42872925880318b3f4bab0e289a6a2ce8b7
Tony Locke <tlocke@tlocke.org.uk>
2015-06-10 23:16:58 +0100

    Removed commented out lines

--- distance/distance.py
+++ distance/distance.py
@@ -16,16 +16,9 @@ cyr = (72966, 98880)
 mart = (73560, 96111)
 james = (75676, 98108)
 
-points = (cyr, mart, james)
-
 
 def dist(p1, p2):
     return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
-
-max_x = max(p[0] for p in points)
-min_x = min(p[0] for p in points)
-max_y = max(p[1] for p in points)
-min_y = min(p[1] for p in points)
 
 error = None
 point = cyr
@@ -45,19 +38,4 @@ for x in range(100000):
         error = err
         point = p
 
-'''
-for x in range(10000001):
-    p = (randint(min_x, max_x), randint(min_y, max_y))
-    # print("looking at point", p)
-    dist_cyr = dist(p, cyr)
-    dist_mart = dist(p, mart)
-    dist_james = dist(p, james)
-    err1 = dist_cyr - dist_mart - 1596
-    err2 = dist_cyr - dist_james - 2233
-    err = err1 ** 2 + err2 ** 2
-    if err < error:
-        print("error", error)
-        error = err
-        best_point = p
-'''
 print("best point", point)
