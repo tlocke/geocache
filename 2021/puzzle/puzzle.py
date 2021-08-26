53add754625d00779f676671ea595dc660fa0e82
Tony Locke <tlocke@tlocke.org.uk>
2021-08-26 18:38:02 +0100

    Collect all results

--- 2021/puzzle/puzzle.py
+++ 2021/puzzle/puzzle.py
@@ -1,9 +1,9 @@
-result = None
+results = []
 for x in range(2000):
     a = 2803 + x / 5
     b = 2655 - x
     s = sum(int(c) for c in f"{a}{b}".replace(".", ""))
     if s == 19:
-        result = x
-        break
-print(result)
+        results.append(x)
+
+print(results)
